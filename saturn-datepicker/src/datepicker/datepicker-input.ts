c8dfa3490fcd595e979996b98dd23bb98c21ccfa
Dmitry Volynkin <saturn@saturn.team>
2018-11-28 08:50:18 +0300

    7.0.1 import dateadapter fix

--- saturn-datepicker/src/datepicker/datepicker-input.ts
+++ saturn-datepicker/src/datepicker/datepicker-input.ts
@@ -29,7 +29,9 @@ import {
   ValidatorFn,
   Validators,
 } from '@angular/forms';
-import {DateAdapter, MAT_DATE_FORMATS, MatDateFormats, ThemePalette} from '@angular/material/core';
+import {DateAdapter} from '../datetime/date-adapter';
+import {MAT_DATE_FORMATS, MatDateFormats} from '../datetime/date-formats';
+import {ThemePalette} from '@angular/material/core';
 import {MatFormField} from '@angular/material/form-field';
 import {MAT_INPUT_VALUE_ACCESSOR} from '@angular/material/input';
 import {Subscription} from 'rxjs';
