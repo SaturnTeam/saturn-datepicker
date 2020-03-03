f040943d9d6ee20b688b3f8229cfe2938b88ffd2
Schneider Werner Walter <contact@schnwalter.eu>
2020-03-03 09:33:40 +0200

    Cleanup app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { RangesFooter } from './ranges-footer.component';
+import { SatDatepickerRangeValue } from '../../saturn-datepicker/src/datepicker';
+
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  form: FormGroup;
-  rangesFooter = RangesFooter;
-  inlineRange;
-  selectedDate;
+export class AppComponent implements OnInit {
+  public formGroup: FormGroup;
+  public inlineRange: SatDatepickerRangeValue<Date>;
+  public rangesFooter = RangesFooter;
+  public selectedDate: Date;
 
-  constructor(fb: FormBuilder) {
-    this.form = fb.group({
-      date: [{begin: new Date(2018, 7, 5), end: new Date(2018, 7, 25)}]
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  public ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      date: [
+        {
+          begin: new Date(2018, 7, 5),
+          end: new Date(2018, 7, 25),
+        },
+      ],
     });
   }
 
-  inlineRangeChange($event) {
-    this.inlineRange = $event;
+  public inlineRangeChange(rangeValue: SatDatepickerRangeValue<Date>): void {
+    this.inlineRange = rangeValue;
   }
-
 }
