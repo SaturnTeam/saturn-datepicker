84d8dfd17c644e33d79e1f4e237ae3bd6c0cb6ae
Schneider Werner Walter <contact@schnwalter.eu>
2020-03-03 09:30:41 +0200

    Cleanup ranges footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,43 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { SatDatepickerModule } from '../../saturn-datepicker/src/datepicker';
 import { SatNativeDateModule } from '../../saturn-datepicker/src/datetime';
+
 import { AppComponent } from './app.component';
 import { RangesFooter } from './ranges-footer.component';
 
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        RangesFooter
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule, ReactiveFormsModule, MatDatepickerModule,
-        MatNativeDateModule, MatFormFieldModule, MatInputModule,
-        MatButtonModule, SatDatepickerModule, SatNativeDateModule
-    ],
-    entryComponents: [RangesFooter],
-    providers: [],
-    bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+  ],
+  declarations: [
+    AppComponent,
+    RangesFooter,
+  ],
+  entryComponents: [
+    RangesFooter,
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    SatDatepickerModule,
+    SatNativeDateModule,
+  ],
 })
 export class AppModule {
 }
--- src/app/ranges-footer.component.ts
+++ src/app/ranges-footer.component.ts
@@ -1,47 +1,74 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { SatCalendar, SatCalendarFooter, SatDatepicker } from '../../saturn-datepicker/src/datepicker';
+import {
+  SatCalendar,
+  SatCalendarFooter,
+  SatDatepicker,
+  SatDatepickerRangeValue,
+} from '../../saturn-datepicker/src/datepicker';
 import { DateAdapter } from '../../saturn-datepicker/src/datetime';
 
+
+export interface IRangesFooterOption {
+  key: string,
+  label: string,
+}
+
 @Component({
-    templateUrl: './ranges-footer.component.html'
+  selector: 'app-ranges-footer',
+  templateUrl: './ranges-footer.component.html',
 })
-export class RangesFooter<Date> implements SatCalendarFooter<Date> {
-    public ranges: Array<{key: string, label: string}> = [
-        {key: 'today', label: 'Today'},
-        {key: 'thisWeek', label: 'This Week'},
-    ];
-    private destroyed = new Subject<void>();
+export class RangesFooter implements OnDestroy, OnInit, SatCalendarFooter<Date> {
+  public rangeOptions: IRangesFooterOption[] = [
+    { key: 'today', label: 'Today' },
+    { key: 'thisWeek', label: 'This Week' },
+  ];
+  private readonly destroyed: Subject<void> = new Subject<void>();
 
-    constructor(
-        private calendar: SatCalendar<Date>,
-        private datePicker: SatDatepicker<Date>,
-        private dateAdapter: DateAdapter<Date>,
-        cdr: ChangeDetectorRef
-    ) {
-        calendar.stateChanges
-            .pipe(takeUntil(this.destroyed))
-            .subscribe(() => cdr.markForCheck())
-    }
+  constructor(
+    private calendar: SatCalendar<Date>,
+    private datePicker: SatDatepicker<Date>,
+    private dateAdapter: DateAdapter<Date>,
+    private changeDetectorRef: ChangeDetectorRef,
+  ) {
+  }
 
-    setRange(range: string) {
-        switch (range) {
-            case 'today':
-                this.calendar.beginDate = this.dateAdapter.deserialize(new Date());
-                this.calendar.endDate = this.dateAdapter.deserialize(new Date());
-                this.calendar.activeDate = this.calendar.beginDate;
-                break;
-            case 'thisWeek':
-                const today = moment();
-                this.calendar.beginDate = this.dateAdapter.deserialize(today.weekday(0).toDate());
-                this.calendar.endDate = this.dateAdapter.deserialize(today.weekday(6).toDate());
-                break;
-        }
+  public ngOnInit(): void {
+    this.calendar.stateChanges.pipe(
+      takeUntil(this.destroyed),
+    ).subscribe((): void => {
+      this.changeDetectorRef.markForCheck();
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroyed.next();
+  }
+
+  public setRange(range: string): void {
+    switch (range) {
+      case 'today':
+        this.calendar.beginDate = this.dateAdapter.deserialize(new Date());
+        this.calendar.endDate = this.dateAdapter.deserialize(new Date());
         this.calendar.activeDate = this.calendar.beginDate;
-        this.calendar.beginDateSelectedChange.emit(this.calendar.beginDate);
-        this.calendar.dateRangesChange.emit({begin: this.calendar.beginDate, end: this.calendar.endDate});
-        this.datePicker.close();
+        break;
+      case 'thisWeek':
+        const today = moment();
+        this.calendar.beginDate = this.dateAdapter.deserialize(
+          today.weekday(0).toDate());
+        this.calendar.endDate = this.dateAdapter.deserialize(
+          today.weekday(6).toDate());
+        break;
     }
+    this.calendar.activeDate = this.calendar.beginDate;
+    this.calendar.beginDateSelectedChange.emit(this.calendar.beginDate);
+    const rangeValue: SatDatepickerRangeValue<Date> = {
+      begin: this.calendar.beginDate,
+      end: this.calendar.endDate,
+    };
+    this.calendar.dateRangesChange.emit(rangeValue);
+    this.datePicker.close();
+  }
 }
